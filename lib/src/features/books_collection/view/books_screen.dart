57c099a5609906a88e312e986bf1eab5e2d36e19
shakshor <shakshor584@gmail.com>
2025-01-27 17:06:24 +0600

    data is showing in books page

--- lib/src/features/books_collection/view/books_screen.dart
+++ lib/src/features/books_collection/view/books_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:words_explorer_app/src/features/books_collection/provider/books_provider.dart';
 import 'package:words_explorer_app/src/features/books_collection/repository/books_list_repo.dart';
 
-class BooksScreen extends StatelessWidget {
+class BooksScreen extends ConsumerWidget {
   const BooksScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncBooks = ref.watch(booksAsyncNotifierProvider);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
@@ -13,13 +17,28 @@ class BooksScreen extends StatelessWidget {
         centerTitle: true,
       ),
 
-      body: Center(
-        child: ElevatedButton(
-          onPressed: (){
-            getBooksList();
-          },
-          child: Text('Future Data'),
-        ),
+      body: Column(
+        children: [
+          // data_check_btn
+          ElevatedButton(
+            onPressed: (){
+              getBooksList();
+            },
+            child: Text('Future Data'),
+          ),
+          // data_show
+          asyncBooks.when(
+              data: (data){
+                return Text(data.totalBooks.toString());
+              },
+              error: (error, stacktrace){
+                return Text(error.toString());
+              },
+              loading: (){
+                return CircularProgressIndicator();
+              }
+          ),
+        ],
       ),
     );
   }
